7572e7db66845eb3d4348f8373bc6ff8bccee601
Enrique José Ramón Balmaseda <ejrbalma@gmail.com>
2017-10-12 13:12:39 +0100

    UPDATE delete comentarios

--- NewKata2/src/newkata2/NewKata2.java
+++ NewKata2/src/newkata2/NewKata2.java
@@ -1,21 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package newkata2;
 
 import java.util.HashMap;
 
-/**
- *
- * @author ejrbalma
- */
 public class NewKata2 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         Integer [] vector = {1, 3, 5, 7, 9, 1, 5, 4, 3, 9,100,-4};
         //String[] vector = {"Ana", "Juan", "Elena", "Elena", "Juan", "Ana", "Pedro"};
