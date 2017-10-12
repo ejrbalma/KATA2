cd02fca119cac5e07847a8b9102c4266963e04da
Enrique José Ramón Balmaseda <ejrbalma@gmail.com>
2017-10-12 18:31:52 +0100

    UPDATE stting Histogram

--- NewKata2/src/newkata2/NewKata2.java
+++ NewKata2/src/newkata2/NewKata2.java
@@ -17,12 +17,12 @@ public class NewKata2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Integer [] vector = {1, 3, 5, 7, 9, 1, 5, 4, 3, 9,100,-4};
-        //String[] vector = {"Ana", "Juan", "Elena", "Elena", "Juan", "Ana", "Pedro"};
+        // Integer [] vector = {1, 3, 5, 7, 9, 1, 5, 4, 3, 9,100,-4};
+        String[] vector = {"Ana", "Juan", "Elena", "Elena", "Juan", "Ana", "Pedro"};
        
-        Histogram <Integer> histogram = CalculaHistogram.computeHistogram(vector);
+        Histogram <String> histogram = CalculaHistogram.computeHistogram(vector);
         
-        for (Integer key : histogram.keySet()) {
+        for (String key : histogram.keySet()) {
             System.out.println(key + " --> " + histogram.get(key));
         }
         
